8c5659f942a64db2cb8be8b111977419f075d51c
shubham1604 <sharmashubh87@gmail.com>
2021-01-03 15:05:36 +0530

    Added pagination on home page

--- core/views.py
+++ core/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import Categories, Products
+from django.core.paginator import Paginator
 
 def home(request):
     context = {}
     categories = Categories.objects.all()[:7]
     products = Products.objects.all()
+
+    #paginator code starts
+    paginator = Paginator(products,9)
+    page = request.GET.get('page',1)
+    products = paginator.get_page(page)
+    #paginator code ends
+
     context.update({'categories':categories, 'products':products})
     return render(request, "home-page.html", context)
 
