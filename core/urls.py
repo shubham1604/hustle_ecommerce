87a0e3dd60e41a49e2d980f67c40101a12a0a7c7
shubham1604 <sharmashubh87@gmail.com>
2021-01-12 18:36:55 +0530

    Integrated stripe payment with the site

--- core/urls.py
+++ core/urls.py
@@ -10,5 +10,6 @@ path('product/<int:pk>/', views.product, name = 'product'),
 path('cart-action', views.cart_action, name = 'cart-action'),
 path('delete-item-from-cart', views.delete_item_from_cart, name = 'delete-item-from-cart'),
 path('order-summary', views.order_summary, name = 'order_summary'),
-path('checkout/', views.checkout, name = 'checkout')
+path('checkout/', views.checkout, name = 'checkout'),
+path('payment/', views.payment, name = 'payment')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
